e6af460405fe3f8acc60a6f9f40c1b58b85ea46f
m4rcous <m.gongora.sanchez08@gmail.com>
2024-11-30 01:32:16 -0500

    feat: first steps in Java with Hola Mundo

---
+++ src/main/java/HolaMundo.java
@@ -0,0 +1,13 @@
+public class HolaMundo {
+    // Agregamos el metodo principal (main)
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo con Java");
+        System.out.println("Saludos");
+    }
+}
+
+/*
+* NOTAS:
+* Con el atajo psvm generamos el metodo main
+* Con el atajo sout generamos un línea de código para poder imprimir en consola
+* */
